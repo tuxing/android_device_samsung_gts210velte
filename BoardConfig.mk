0a8ce610c3880b0917ef286ff551d9aef53e95e9
tuxing <tuxing@gmail.com>
2017-10-17 15:43:52 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,7 @@ DEVICE_PATH := device/samsung/gts210velte
 TARGET_OTA_ASSERT_DEVICE := gts210velte
 
 # Board
-TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+# TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 
 # Telephony
 BOARD_RIL_CLASS := ../../../device/samsung/gts210velte/ril
